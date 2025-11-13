c446713203e9b9a113e2e505ff50e80a74f0659a
Sofia <swl24@inf.ufpr.br>
2025-11-13 14:28:14 -0300

    [fix] fixed execution of Monte Carlos Integral

--- ex_Int_MontCarlo/labMonteCarlo.c
+++ ex_Int_MontCarlo/labMonteCarlo.c
@@ -10,6 +10,7 @@
 #define SRAND(a) srandom(a) // srand48(a)
 #define RAND_AB(a, b) ((a) + NRAND * ((b) - (a))) // random entre [a,b]
 
+/*
 inline real_t f(real_t *x, int dim){
   real_t resultado = 0.0;
   
@@ -22,6 +23,7 @@ inline real_t f(real_t *x, int dim){
 
   return resultado;
 }
+*/
 
 // Integral Monte Carlo da função Styblinski-Tang de 2 variáveis
 real_t styblinskiTang(real_t a, real_t b, int namostras, int dim)
@@ -31,40 +33,52 @@ real_t styblinskiTang(real_t a, real_t b, int namostras, int dim)
     return 0.0;
   }
     
-  real_t resultado;
-  real_t x, soma = 0.0;
+  real_t resultado = 0.0;
+  real_t x, qx, soma = 0.0;
   
   printf("Metodo de Monte Carlo (x, y).\n");
-  printf("a = (%f), b = (%f), n = (%d), variaveis = 2\n", a, b, namostras);
+  printf("a = (%f), b = (%f), n = (%d), variaveis = %d\n", a, b, namostras, dim);
   
   rtime_t t_inicial = timestamp();
 
   real_t areaAB = 1.0;
-  real_t *aleats = malloc(sizeof(real_t)*dim);
-
   
   // 2, 4, ou 8 DIMENSÕES
   for (int i=0; i<dim; i++){
     areaAB *= (b-a);
-    aleats[i] = RAND_AB(a, b);
   }
 
-  if (dim =2){
-    for (int i=0; i<namostras; i++){
-      // x = a + h
-      // 'h' é uma porcentagem aleat do tamanho do intervalo [a, b]
-      x = a + NRAND * areaAB;
-      soma += f(aleats, dim); 
+  int h = b-a;
+  /* 
+     Solução genérica para todas dimensões
+     Se fossemos fazer uma solução específica poderíamos ter: x, y, qx, qy  
+  */
+  real_t imc = 0.0;
+  for (int i=0; i<namostras; i++){
+    // x = a + h
+    // 'h' é uma porcentagem aleat do tamanho do intervalo [a, b]
+    imc += soma;
+    for (int j=0; j<dim; j++){
+      x = a + NRAND * h;  
+      qx = x*x;
+      soma += qx*qx - 16*qx + 5*x; 
     }
+    soma /= 2.0;
   }
+  
 
-  resultado = (soma / namostras) * areaAB;
+  resultado = (imc / namostras) * areaAB;
   
   rtime_t t_final = timestamp();
   printf("Tempo decorrido: %f seg.\n", t_final - t_inicial);
   
-  free(aleats);
   return resultado;
+
+  /* OTIMIZANDO:  
+    Unroll and jam das somas
+    Soma somas no final
+    resultado = (somaS / namostras) * areaAB
+  */
 }
 
 // 2 DIMENSÕES
@@ -74,25 +88,22 @@ real_t retangulos_xy(real_t a, real_t b, int npontos) {
   real_t h = (b-a)/ k;
   real_t resultado;
   real_t soma = 0;
-  real_t *x, *y;
-  y = malloc(sizeof(real_t)*k);
-  x = malloc(sizeof(real_t)*k);
-  for (int i=0; i<k; i++){
-    y[i] = RAND_AB(a, b);
-    x[i] = RAND_AB(a, b);
-  }
+  real_t x, y, qx, qy; 
 
   printf("Metodo dos Retangulos (x, y).\n");
   printf("a = (%f), b = (%f), n = (%d), h = (%lg)\n", a, b, npontos, h);
   
   rtime_t t_inicial = timestamp();
   
-  for (int i=1; i<npontos; i++)
-    for (int j=1; j<npontos; j++){
-      resultado += f(x, npontos); 
+  for (int i=a; i<=b; i+=h){
+    qx = x*x;
+    for (int j=a; j<=b; j+=h){
+      qy = y*y;
+      soma += (qx*qx + qy*qy -16*(qx+qy) +5*(x+y))/2; 
     }
-      
-  resultado *= h*h;
+  }
+
+  resultado = h*h*soma;
 
   rtime_t t_final = timestamp();
   printf("Tempo decorrido: %f seg.\n", t_final - t_inicial);
